ea83f5054481d5813f988dacf85c5b6b1686ede8
sunalex <sunalex@sunalex.ru>
2016-05-01 20:10:16 +0300

    Base markup

---
+++ js/scripts.js
@@ -0,0 +1,63 @@
+$(document).ready(function () {
+
+    $('.alert').hide();
+
+    $(function () {
+        $('.close-alert').click(function () {
+            $('.alert').hide();
+        });
+    });
+
+    $(".form").each(function (formIndex, formElement) {
+        $(formElement).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: {
+                    required: true,
+                    minlength: 7
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, корректно заполните поле.",
+                    minlength: "Введите больше 2 символов."
+                },
+                phone: {
+                    required: "Пожалуйста, корректно заполните поле.",
+                    minlength: "Введите больше 7 символов."
+                }
+            },
+            submitHandler: function (form) {
+                var $form = $(form);
+                $.post('send.php', $form.serialize(), function (data) {
+                    $('.form-control').removeClass('valid');
+                    $(document).ready(function () {
+                        $('.form-control').val('');
+                        $('.modal').modal('hide');
+
+                        $('.alert').show();
+
+                        $(function () {
+                            $(".request").animate({ scrollTop: 0 }, "slow");
+                            return false;
+                        });
+
+                    });
+                });
+            }
+        });
+    });
+
+
+        $('.to-bottom').click(function(){
+
+            $('html, body').animate({
+                scrollTop: $(".request").offset().top
+            }, 300);
+
+        });
+
+});
