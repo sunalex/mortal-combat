9fa0bbd26f1ec71dbcd66cf755ae01503b8a91b4
sunalex <sunalex@sunalex.ru>
2016-05-05 15:15:48 +0300

    Script update

--- send.php
+++ send.php
@@ -6,11 +6,9 @@ if (isset($_POST['submit'])) {
         }
         $name = $_POST['name'];
         $phone = $_POST['phone'];
-        $city = $_POST['city'];
-        $address = $_POST['address'];
         $to = 'sunalex@sunalex.ru'; //TODO
-        $subject = 'Заказ на тактический передник!';
-        $message = 'Имя: '.$name. ', Телефон: '.$phone. ', Время: ' .$time. ', Город: ' .$city. ', Адрес: ' .$address;
+        $subject = 'Запрос на участие в переговорных поединках';
+        $message = 'Имя: '.$name. ', Телефон: '.$phone. ', Время: ' .$time. ';
 
         $headers = 'Content-type: text/html; charset=UTF-8\r\n';
 
