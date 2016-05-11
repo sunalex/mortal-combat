a2a75274cdf291d03a2ef53029bbc78f26e5889e
sunalex <sunalex@sunalex.ru>
2016-05-11 13:43:34 +0300

    Remove time from the script

--- send.php
+++ send.php
@@ -1,14 +1,11 @@
 <?php
 
 if (isset($_POST['submit'])) {
-        if(!empty($_POST['time'])){
-            $time = $_POST['time'];
-        }
         $name = $_POST['name'];
         $phone = $_POST['phone'];
         $to = 'sunalex@sunalex.ru'; //TODO
         $subject = 'Запрос на участие в переговорных поединках';
-        $message = 'Имя: '.$name. ', Телефон: '.$phone. ', Время: ' .$time;
+        $message = 'Имя: '.$name. ', Телефон: '.$phone;
 
         $headers = 'Content-type: text/html; charset=UTF-8\r\n';
 
